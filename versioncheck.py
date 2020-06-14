d541eedabf073857bd7fb0ccf3c6a0292d57682d
smaxkz <smaxkz@gmail.com>
2020-06-14 12:38:20 +0200

    примеры использования модулей стандартной библиотеки

---
+++ versioncheck.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, warnings
+
+if sys.version_info[0] < 3:
+    warnings.warn("Для выполнения этой программы необходима как минимум \
+                  версия Python 3.0", 
+            RuntimeWarning)
+else:
+    print('Нормальное продолжение')
